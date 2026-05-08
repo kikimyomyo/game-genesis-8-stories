e224770c565ad63502a39f1b9d978a838d70cc17
kikimyomyo <request1004@gmail.com>
2026-05-08 16:16:20 +0900

    deploy: genesis-8-stories

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "genesis-8-stories",
+  title: "창세기 8장면 — 창조부터 요셉까지",
+  sheet_tab: "genesis-8-stories",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
